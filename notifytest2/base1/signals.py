7be4cfe4707d269f47f6a8e939b3a49a422b5b7b
SheikhFahan <business.fahan@gmail.com>
2023-03-15 22:23:25 +0530

    database connection changes RW

--- notifytest2/base1/signals.py
+++ notifytest2/base1/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import Professor
-from django_plpy.installer import pltrigger
 
 
 # @pltrigger(event="INSERT", when="AFTER", model=User)
