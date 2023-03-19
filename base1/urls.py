49843072781388a00a2d4d22298087e0379d2437
SheikhFahan <business.fahan@gmail.com>
2023-03-19 14:41:38 +0530

    changes for debug

--- base1/urls.py
+++ base1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static, serve
+from django.urls import re_path
 urlpatterns = [
     path("", views.indexPage, name="index"),
     path("about_us/", views.aboutUs, name="aboutUs"),
@@ -27,11 +28,14 @@ urlpatterns = [
     path("view_assignments/", views.viewAssignments, name = "viewAssignments"),
     path("views_questions/", views.viewQuestions, name = "viewQuestions"),
 
-    path("resc/<str:pk>/", views.pdfSearch, name = 'customPdf')
+    path("resc/<str:pk>/", views.pdfSearch, name = 'customPdf'),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 
 
 ]
-urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
