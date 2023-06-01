60f69514f6ec0c58692bb06c69006b5a474a3af6
SheikhFahan <business.fahan@gmail.com>
2023-06-01 16:03:37 +0530

    Revert "admin page style"
    This reverts commit 415570b05f03b885ddf53f1c276dfaa492fbcb07.

--- notifytest2/test.py
+++ notifytest2/test.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-import os
 
 class NotifyConfigTest(TestCase):
     def test_temp(self):
-        print(os.environ.get('SECRET_KEY'))
+        self.assertTrue(1==1)
